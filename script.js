85de20efdb667e9bd06ca659bd3973017ab9e300
ninadinh31 <ninadinh31@gmail.com>
2017-12-26 08:18:44 -0500

    update script

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 window.focus();
 
 $(document).ready(function() {
-    console.log( "ready!" );
-
     var interval_id;
     if (!interval_id) {
         interval_id = setInterval(function () {
@@ -11,8 +9,6 @@ $(document).ready(function() {
     }
 
     $(window).focus(function() {
-        console.log ("here");
-
         interval_id = setInterval(function () {
             $('.ui.text.shape').shape('flip up');
         }, 1500);
